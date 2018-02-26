5cae25c6f357124ae615dc9acb71a5ba8c302a6f
thakkarj <thakkarj@student.unimelb.edu.au>
2018-02-27 01:15:27 +1100

    UI Completed

--- capgeminiTest/ViewController.swift
+++ capgeminiTest/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var navitem: UINavigationItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navitem.title = "jigar"
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +27,37 @@ class ViewController: UIViewController {
     }
 
 
+    
+
+    // MARK: User-defined methods
+//    //-- A method to resize the image before display in a tableview' cell
+//    func image(with image: UIImage, scaledTo newSizeOfImage: CGSize) -> UIImage {
+//        //UIGraphicsBeginImageContext(newSize);
+//        // In next line, pass 0.0 to use the current device's pixel scaling factor (and thus account for Retina resolution).
+//        // Pass 1.0 to force exact pixel size.
+//        let size: CGSize = image.size
+//        let widthRatio: CGFloat = newSizeOfImage.width / image.size.width
+//        let heightRatio: CGFloat = newSizeOfImage.height / image.size.height
+//        // Figure out what our orientation is, and use that to form the rectangle
+//
+////        if widthRatio > heightRatio {
+////            newSizeOfImage = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+////        }
+////        else {
+////            newSizeOfImage = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
+////        }
+//
+//        // This is the rect that we've calculated out and this is what is actually used below
+//        let rect = CGRect(x: 0, y: 0, width: newSizeOfImage.width, height: newSizeOfImage.height)
+//        // Actually do the resizing to the rect using the ImageContext stuff
+//        UIGraphicsBeginImageContextWithOptions(newSizeOfImage, false, 0.0)
+//        image.draw(in: rect)
+//        let newImage: UIImage? = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        return newImage ?? UIImage()
+//    }
+
+    
+    
 }
 
---
+++ capgeminiTest/canadaInfoTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  canadaInfoTableViewCell.swift
+//  capgeminiTest
+//
+//  Created by Jigar Thakkar on 27/2/18.
+//  Copyright © 2018 Jigar Thakkar. All rights reserved.
+//
+
+import UIKit
+
+class canadaInfoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imagefromHref: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var descriptionText: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
