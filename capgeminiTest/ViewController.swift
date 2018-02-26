239d548ca9908e6729800661b8e91b13a524724b
thakkarj <thakkarj@student.unimelb.edu.au>
2018-02-27 00:17:04 +1100

    Initial Commit

---
+++ capgeminiTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  capgeminiTest
+//
+//  Created by Jigar Thakkar on 27/2/18.
+//  Copyright © 2018 Jigar Thakkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
